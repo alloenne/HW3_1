560fb57a8cd94a568e90054a70672eb3eeefde72
Julia Rubina <jl_rubina@mail.ru>
2022-12-21 23:22:54 +0300

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,8 @@
 public class BonusMilesService {
-    public int calculate(int cost){
+    public int calculate(int cost) {
         int amauntMiles = cost / 20;
         return amauntMiles;
     }
-
-
 
 
 }
